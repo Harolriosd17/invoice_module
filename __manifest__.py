a97c2d67b4768275aa0b5dcaa2676110691f8741
Harol Rios <harol.rios@life1.us>
2024-11-19 10:29:35 -0500

    modificacion datos manifest.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,9 +1,9 @@
 {
-    'name': 'Mi Módulo de Facturas',
+    'name': 'Facturación Panamá',
     'version': '16.0.1.0.0',
     'category': 'Accounting',
     'summary': 'Extiende el formulario de facturas en Odoo 16',
-    'author': 'Tu Nombre',
+    'author': 'Harol Rios',
     'depends': ['account'],  # Asegúrate de que dependa del módulo de contabilidad
     'data': [
         'views/invoice_view.xml',  # Vista extendida de la factura
